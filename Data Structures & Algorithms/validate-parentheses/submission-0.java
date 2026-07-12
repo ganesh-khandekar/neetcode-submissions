a19bb55d38e4807b90bfcbe8cbe40562efef03ee
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-12 22:58:07 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> Parentheses =new Stack<>();
+            // System.out.println(Parentheses);
+        for(char ch :s.toCharArray()){
+            if(ch=='['||ch=='('||ch=='{'){
+                Parentheses.push(ch);
+            }else if(ch==']'&&Parentheses.peek()=='['){
+                Parentheses.pop();
+            }else if(ch=='}'&&Parentheses.peek()=='{'){
+                Parentheses.pop();
+            }else if(ch==')'&&Parentheses.peek()=='('){
+                Parentheses.pop();
+            }else{
+                return false;
+            }
+            System.out.println(" ch"+ch);
+        }
+            System.out.println(Parentheses);
+        return true;
+    }
+}
