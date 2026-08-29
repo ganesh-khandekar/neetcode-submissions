8d412307bd435dcadc992470c5cafa32a09d36f2
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-08-29 11:40:45 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean check(int n, int[] p, int h) {
+        int count = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] <= n) {
+                count++;
+            } else if (p[i] % n == 0) {
+                count = count + (p[i] / n);
+            } else {
+                count = count + (p[i] / n);
+                count++;
+            }
+        }
+        if (h < count) {
+            return false;
+        }
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int j = Arrays.stream(piles).max().getAsInt(), i = 1;
+        int mid = 1;
+        while (i < j) {
+            mid = i + ((j - i) / 2);
+            if (check(mid, piles, h)) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+}
