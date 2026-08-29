7da9d7faa96b279905bfb7a620882ee951bc9e90
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-08-29 11:56:12 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums.length==0){
+            return false;
+        }
+        Hashtable<Integer, Boolean> flag = new Hashtable<>();
+        flag.put(nums[0],true);
+
+        for(int i=1; i<nums.length;i++){
+            if(!flag.containsKey(nums[i])){
+                flag.put(nums[i],true);
+            }else{
+                return true;
+            }
+        }
+        return false ;
+    }
+}
